420bc2f31934db5a1b14da377ad5a18a24952ce8
noboKumar <nobokumar227@gmail.com>
2025-05-18 13:04:53 +0600

    add firebase auth context and signin setup

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const SignIn = () => {
+  const { createUser } = use(AuthContext);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    // create User in Firebase
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="my-20">
-      <form className="mx-auto fieldset bg-base-200 border-base-300 w-xs border px-12 py-6 shadow space-y-2 rounded-2xl">
+      <form
+        onSubmit={handleSignIn}
+        className="mx-auto fieldset bg-base-200 border-base-300 w-xs border px-12 py-6 shadow space-y-2 rounded-2xl"
+      >
         <h1 className="text-4xl font-semibold">Sign In Now!</h1>
 
         <div>
@@ -12,6 +34,7 @@ const SignIn = () => {
             type="email"
             className="input border-2 rounded"
             placeholder="Email"
+            name="email"
           />
         </div>
 
@@ -21,6 +44,7 @@ const SignIn = () => {
             type="password"
             className="input  border-2 rounded"
             placeholder="Password"
+            name="password"
           />
         </div>
 
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
 };
 
