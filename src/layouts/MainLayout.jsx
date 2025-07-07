43875dad9f63a136df8692d48090885d9e02fed5
noboKumar <nobokumar227@gmail.com>
2025-07-07 10:17:50 +0600

    remove custom cursor

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar";
-import CustomCursor from "../components/UI/CustomCursor";
 
 const MainLayout = () => {
   return (
     <div>
-      <CustomCursor />
       <NavBar></NavBar>
       <Outlet></Outlet>
     </div>
